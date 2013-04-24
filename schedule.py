6a04c90f7baf7e96393a115b29c9efb083d96ab2
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-04-24 23:38:53 +0100

    Print team enumeration members

--- schedule.py
+++ schedule.py
@@ -3,3 +3,12 @@
 print "(set-info :status unknown)"
 print "(set-option :produce-models true)"
 print "; Logic is now \"Whatever Z3 accepts\" (set-logic AUFBV)"
+print ""
+
+# Configurable number of enum members
+
+print "(declare-datatypes () ((TEAM "
+for i in range(12):
+	print "t{0}".format(i),
+
+print ")"
