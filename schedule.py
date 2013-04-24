edb964447afafa1da976a39349b517da4a06e01c
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-04-24 23:53:18 +0100

    Parameterise some bit widths

--- schedule.py
+++ schedule.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+NUMTEAMS = 12
+ROUNDBITS = 4
+MATCHBITS = 4
+SLOTBITS = 2
+
 print "(set-info :status unknown)"
 print "(set-option :produce-models true)"
 print "; Logic is now \"Whatever Z3 accepts\" (set-logic AUFBV)"
@@ -8,7 +13,7 @@ print ""
 # Configurable number of enum members
 
 print "(declare-datatypes () ((TEAM "
-for i in range(12):
+for i in range(NUMTEAMS):
 	print "t{0}".format(i),
 
 print ")"
@@ -16,5 +21,5 @@ print ")"
 # The uninterpreted function that's going to become our scheduler. Takes a
 # 4 bit round, 4 bit match, 2 bit slot, returns a team.
 print ""
-print "(declare-fun sparticus ((_ BitVec 4) (_ BitVec 4) (_ BitVec 2)) TEAM)"
+print "(declare-fun sparticus ((_ BitVec {0}) (_ BitVec {1}) (_ BitVec {2})) TEAM)".format(ROUNDBITS, MATCHBITS, SLOTBITS)
 print ""
