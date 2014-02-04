1b0d39627fa75d02a8ab549c5ff57c4bdf5bd022
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-04 17:41:14 +0000

    Attempt to bake a small lisp parser.

--- readresult.py
+++ readresult.py
@@ -2,6 +2,9 @@
 
 import sys
 
+from pyparsing import nums, oneOf, Word, Literal, Suppress, alphas
+from pyparsing import ParseException, Forward, Group, OneOrMore
+
 # This expects to read one line containing the status ("sat"), then a bunch of
 # lisp expressions produced by the get-value queries at the end of the script.
 
@@ -11,6 +14,29 @@ line = line[0:-1] # chop newline
 if line == "unsat":
 	print "You've won a prize: the formula is unsatisfiable!"
 	sys.exit(1)
-else if line != "sat":
+elif line != "sat":
 	print "Unrecognized get-sat result: {0}".format(line)
 	sys.exit(1)
+
+# Build a pyparsing thing.
+
+newline = Literal('\n')
+lparen = Literal('(')
+rparen = Literal(')')
+symbol = Word(alphas + "_" + "0123456789") # Not the whole smtlib range, but meh
+
+# Our expr:
+expr = Forward()
+expr << Group(lparen + symbol + expr + rparen)
+expr << Group(lparen + symbol + expr + expr + rparen)
+expr << Group(lparen + symbol + expr + expr + expr + rparen)
+
+# Pairs of results for the output.
+outpair = Group(lparen + lparen + expr + expr + rparen + rparen + newline)
+everything = OneOrMore(outpair)
+
+# Read /all the things/
+
+inputblob = sys.stdin.read(1000000)
+foo = everything.parseString(inputblob)
+print repr(foo)
