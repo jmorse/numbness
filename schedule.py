b7c3afd9b30876794df9cbce050bf2d04cb88280
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-04 16:56:39 +0000

    Write a QF_BV model writer.

--- schedule.py
+++ schedule.py
@@ -79,11 +79,30 @@ class QFAUFBV:
 
 	pass
 
+class QFBV:
+	def preamble(self):
+		print "(set-logic QF_BV)"
+
+		print ""
+
+		# Enumerate all the variables, for each match round.
+		for i in range(NUMROUNDS):
+			for j in range(NUMMATCHES):
+				for k in range(NUMSLOTS):
+					print "(declare-fun {0} () (_ BitVec {1}))".format(self.project(i, j, k), TEAMBITS)
+
+	def project(self, x, y, z):
+		return "round_{0}_match_{1}_slot_{2}".format(x, y, z)
+
+	pass
+
 output_object = None
 if USE_Z3:
 	output_object = Z3()
 else:
 	output_object = QFAUFBV()
+
+output_object = QFBV()
 
 output_object.preamble()
 
