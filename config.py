8e0150dad46cb3d025c0165ef1f3de113a7900fc
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-03-09 18:52:21 +0000

    Derive bitwidths from match configuration

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import sys
+from math import log, ceil
 
 # More flexible parameters
 NUMROUNDS = 13                  # How many rounds to schedule in the competition
@@ -7,14 +8,16 @@ CLOSENESS = 5                   # Minimum number of matches between each teams
                                 # appearance.
 
 # More built in parameters.
-NUMSLOTS = 4
+NUMSLOTS = 4                    # Everything will break if this changes.
+                                # That's fine, because SR doesn't use any other
+                                # match format for the moment.
 NUMMATCHES = NUMTEAMS / NUMSLOTS
 
-# Underlying bitwidths
-ROUNDBITS = 4
-MATCHBITS = 4
-SLOTBITS = 2
-TEAMBITS = 6
+# Underlying bitwidths, computed from other parameters
+ROUNDBITS = int(ceil(log(NUMROUNDS, 2)))  # Num bits required for n rounds
+MATCHBITS = int(ceil(log(NUMMATCHES, 2))) # Same, for matches
+SLOTBITS = int(ceil(log(NUMSLOTS, 2)))    # ...
+TEAMBITS = int(ceil(log(NUMTEAMS, 2)))    # ...
 
 # Validation
 
