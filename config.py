7058b0ab207eb5f9a06fd3ce96b82de696cd2224
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-03-09 18:58:09 +0000

    Recompute bitwidths in a function.
    Also global up all the things.

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
 from math import log, ceil
 
+global NUMROUNDS, NUMTEAMS, CLOSENESS, NUMSLOT
+
 # More flexible parameters
 NUMROUNDS = 13                  # How many rounds to schedule in the competition
 NUMTEAMS = 32                   # The number of teams taking part
@@ -11,13 +13,18 @@ CLOSENESS = 5                   # Minimum number of matches between each teams
 NUMSLOTS = 4                    # Everything will break if this changes.
                                 # That's fine, because SR doesn't use any other
                                 # match format for the moment.
-NUMMATCHES = NUMTEAMS / NUMSLOTS
 
-# Underlying bitwidths, computed from other parameters
-ROUNDBITS = int(ceil(log(NUMROUNDS, 2)))  # Num bits required for n rounds
-MATCHBITS = int(ceil(log(NUMMATCHES, 2))) # Same, for matches
-SLOTBITS = int(ceil(log(NUMSLOTS, 2)))    # ...
-TEAMBITS = int(ceil(log(NUMTEAMS, 2)))    # ...
+def compute_bitwidths():
+    global NUMMATCHES, ROUNDBITS, MATCHBITS, SLOTBITS, TEAMBITS
+    NUMMATCHES = NUMTEAMS / NUMSLOTS
+
+    # Underlying bitwidths, computed from other parameters
+    ROUNDBITS = int(ceil(log(NUMROUNDS, 2)))
+    MATCHBITS = int(ceil(log(NUMMATCHES, 2)))
+    SLOTBITS = int(ceil(log(NUMSLOTS, 2)))
+    TEAMBITS = int(ceil(log(NUMTEAMS, 2)))
+
+compute_bitwidths()
 
 # Validation
 
